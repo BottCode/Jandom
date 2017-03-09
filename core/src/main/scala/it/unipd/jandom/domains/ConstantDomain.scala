6d9c29d4939fe6df1384af31c0168b9535ce87a3
StefanoMunari <stefanomunari.sm@gmail.com>
2017-03-09 16:51:33 +0100

    add skeleton for companion object

---
+++ core/src/main/scala/it/unipd/jandom/domains/ConstantDomain.scala
@@ -0,0 +1,14 @@
+package it.unipd.jandom.domains
+
+import it.unich.jandom.domains.numerical.NumericalDomain
+
+
+/**
+  * Constant domain
+  *
+  * @author Mirko Bez <mirko.bez@studenti.unipd.it>, Sebastiano Valle <sebastiano.valle@studenti.unipd.it>
+  *           Stefano Munari <stefano.munari.1@studenti.unipd.it>
+  */
+object ConstantDomain extends NumericalDomain{
+// todo: implement companion object operations
+}

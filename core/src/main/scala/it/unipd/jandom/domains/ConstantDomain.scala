830454cdc40a3792043469bb830a3deb725356a3
StefanoMunari <stefanomunari.sm@gmail.com>
2017-03-09 19:15:43 +0100

    add skeleton: apply, top, bottom, isPolyhedral, addVAriable

--- core/src/main/scala/it/unipd/jandom/domains/ConstantDomain.scala
+++ core/src/main/scala/it/unipd/jandom/domains/ConstantDomain.scala
@@ -1,7 +1,7 @@
 package it.unipd.jandom.domains
 
-import it.unich.jandom.domains.numerical.NumericalDomain
-
+import it.unich.jandom.domains.numerical.{NumericalDomain, NumericalProperty}
+import it.unipd.jandom.domains.ConstantDomainCore._
 
 /**
   * Constant domain
@@ -10,5 +10,29 @@ import it.unich.jandom.domains.numerical.NumericalDomain
   *           Stefano Munari <stefano.munari.1@studenti.unipd.it>
   */
 object ConstantDomain extends NumericalDomain{
-// todo: implement companion object operations
+
+  def apply(constants : Array[Constant]): Property = Property(constants, constants.forall( _.equals(ConstantBottom)))
+
+  def top(num: Int) = Property(Array.fill(num)(ConstantTop), unreachable = false)
+
+  def bottom(num: Int) = Property(Array.fill(num)(ConstantBottom), unreachable = true)
+
+  case class Property private[ConstantDomain] (constants: Array[Constant], unreachable: Boolean) extends NumericalProperty[Property] {
+
+    override type Domain = ConstantDomain.type
+
+    /**
+      * @inheritdoc
+      */
+    override def isPolyhedral: Boolean = false
+
+    /**
+      * @inheritdoc
+      */
+    override def addVariable(): Property =
+      if (unreachable)
+        ConstantDomain.this.bottom(dimension + 1)
+      else
+        ConstantDomain.this(Constant :+ ConstantTop)
+  }
 }

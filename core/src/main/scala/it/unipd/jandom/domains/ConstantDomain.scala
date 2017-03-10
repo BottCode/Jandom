0bbdab01a95535da9603a5171bfe27a8b7e0efad
StefanoMunari <stefanomunari.sm@gmail.com>
2017-03-10 18:09:02 +0100

    define delVariable, mapVariables

--- core/src/main/scala/it/unipd/jandom/domains/ConstantDomain.scala
+++ core/src/main/scala/it/unipd/jandom/domains/ConstantDomain.scala
@@ -2,6 +2,8 @@ package it.unipd.jandom.domains
 
 import it.unich.jandom.domains.numerical.{NumericalDomain, NumericalProperty}
 import it.unipd.jandom.domains.ConstantDomainCore._
+import it.unipd.jandom.domains.constantsDomain.Property
+import it.unipd.jandom.domains.constantsDomainCore.constants
 
 /**
   * Constant domain
@@ -43,14 +45,33 @@ object ConstantDomain extends NumericalDomain{
     def bottom: Property = ConstantDomain.bottom(constants.length)
 
     def top: Property = ConstantDomain.top(constants.length)
-
-    /**
-      * @inheritdoc
-      */
+    
     override def addVariable(): Property = {
       if (unreachable)
         return ConstantDomain.this.bottom(dimension + 1)
       ConstantDomain.this(constants :+ ConstantTop)
     }
+    
+    override def delVariable(index: Int): Property = {
+      require(index < constants.length && index >= 0)
+      val result = new Array[Constant](constants.length - 1)
+      // Copy the first index-1 elements
+      Array.copy(constants, 0, result, 0, index)
+      // Copy the remaining elements
+      Array.copy(constants, index + 1, result, index, constants.length - index - 1)
+      Property(result, unreachable)
+    }
+    
+    def mapVariables(rho: Seq[Int]): Property =  {
+      require(rho.length == dimension)
+      val resultDim = rho.count(_ >= 0)
+      require(rho forall { i => i >= -1 && i < resultDim })
+      // we do not check injectivity
+      val result = new Array[Constant](resultDim)
+      for ((resultIndex, i) <- rho.zipWithIndex; if resultIndex >= 0)
+        result(resultIndex) = constants(i)
+      Property(result, unreachable)
+    }
+    
   }
 }

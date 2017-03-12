b184c8412dff3ae44a1f3ac2324bab8614250ed3
Sebastiano <sebastiano.valle@studenti.unipd.it>
2017-03-12 10:58:14 +0100

    Add algebraic operations to ES01 core

--- core/src/main/scala/it/unipd/jandom/domains/numerical/ExtendedSigns01Core.scala
+++ core/src/main/scala/it/unipd/jandom/domains/numerical/ExtendedSigns01Core.scala
@@ -19,4 +19,77 @@ object ExtendedSigns01Core {
     if (n < 0)  Negative
     GreaterThanOne
   }
+
+  def sum(s : ExtendedSign01, t : ExtendedSign01) : ExtendedSign01 = {
+    (s,t) match {
+      case (ES01Bottom, _) => ES01Bottom
+      case (_, ES01Bottom) => ES01Bottom
+      case (ES01Top, _) => ES01Top
+      case (_, ES01Top) => ES01Top
+      case (a, Zero) => a
+      case (Zero, a) => a
+      case (One, One) => GreaterThanOne
+      case (GreaterThanOne, One) => GreaterThanOne
+      case (One, GreaterThanOne) => GreaterThanOne
+      case (GreaterThanOne, GreaterThanOne) => GreaterThanOne
+      case _ => ES01Top
+    }
+  }
+
+  def mult(s : ExtendedSign01, t : ExtendedSign01) = {
+    (s, t) match {
+      case (ES01Bottom, _) => ES01Bottom
+      case (_, ES01Bottom) => ES01Bottom
+      case (a, Zero) => Zero
+      case (Zero, a) => Zero
+      case (a, One) => a
+      case (One, a) => a
+      case (Negative, Negative) => ES01Top
+      case (Negative, GreaterThanOne) => Negative
+      case (GreaterThanOne, Negative) => Negative
+      case (GreaterThanOne, GreaterThanOne) => GreaterThanOne
+      case _ => ES01Top
+    }
+  }
+
+    def inverse(s : ExtendedSign01) = {
+      s match {
+        case GreaterThanOne => Negative
+        case One => Negative
+        case Negative => ES01Top
+        case a => a
+      }
+  }
+
+  def division(s : ExtendedSign01, t : ExtendedSign01) = {
+    (s,t) match {
+      case (ES01Bottom, _) => ES01Bottom
+      case (_, ES01Bottom) => ES01Bottom
+      case (_, Zero) => ES01Bottom
+      // we already took care of a / 0
+      case (Zero, _) => Zero
+        // a / 1 = a
+      case (a, One) => a
+      case (One, Negative) => Negative
+      case (One, GreaterThanOne) => Zero
+      // (2 / 5) = 0   ---   (5 / 5) = 1   ---   (10 / 5) = 2
+      case (GreaterThanOne, GreaterThanOne) => ES01Top
+      // (-2 / -5) = 0   ---   (-4 / -2) = 2
+      // (1 / -5) = 0   ---   (1 / -1) = -1
+      // (5 / -5) = -1   ---   (2 / -5) = 0
+      case (_, Negative) => ES01Top
+    }
+  }
+
+  def remainder(s: ExtendedSign01, t : ExtendedSign01) : ExtendedSign01 = {
+    (s,t) match {
+      case (ES01Bottom, _) => ES01Bottom
+      case (_, ES01Bottom) => ES01Bottom
+      case (_, Zero) => ES01Bottom
+      case (_, One) => Zero
+      case (Zero, _) => Zero
+      case (One, GreaterThanOne) => One
+      case _ => ES01Top
+    }
+  }
 }

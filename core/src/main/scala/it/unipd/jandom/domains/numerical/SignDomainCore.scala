025fcf90ac5a4c4c7ede3f299d8222a0e692a419
Sebastiano <sebastiano.valle@studenti.unipd.it>
2017-03-13 21:53:37 +0100

    Cosmetic changes to SignDomainCore
    Docs & minor refactoring

--- core/src/main/scala/it/unipd/jandom/domains/numerical/SignDomainCore.scala
+++ core/src/main/scala/it/unipd/jandom/domains/numerical/SignDomainCore.scala
@@ -1,35 +1,59 @@
 package it.unipd.jandom.domains.numerical
 
+/**
+  * Static class that describes operations on the sign domain.
+  *
+  * @author Mirko Bez <mirko.bez@studenti.unipd.it>, Sebastiano Valle <sebastiano.valle@studenti.unipd.it>
+  *           Stefano Munari <stefano.munari@studenti.unipd.it>
+  */
 object SignDomainCore {
 
   trait Sign
+  // positive numbers (> 0)
   case object Plus extends Sign
+  // negative numbers (< 0)
   case object Minus extends Sign
+  // null numbers (= 0)
   case object Zero extends Sign
+  // no accurate info available for variable
   case object SignTop extends Sign
+  // no possible value
   case object SignBottom extends Sign
 
-  def toSign(n : Int) : Sign = {
+  /**
+    * Factory method for signs.
+    * @param n number that has to be converted to sign
+    * @return sign of `n`
+    */
+  def toSign(n : Int) : Sign =
     if(n < 0)
       Minus
     else if(n == 0)
       Zero
     else
       Plus
-  }
 
-  def toSign(num : Double): Sign = {
+  /**
+    * Factory method for signs.
+    * @param num number that has to be converted to sign
+    * @return sign of `num`
+    */
+  def toSign(num : Double): Sign =
     if(num > 0)
       Plus
     else if(num < 0)
       Minus
-    else //Ignoring precision for now!
+    else
       Zero
-  }
 
 
-  def sum(s: Sign, t: Sign) : Sign = {
-    println("sum called ")
+  /**
+    * Returns the sum of two sign variables.
+    * @param s the first term of the addition
+    * @param t the second term of the addition
+    * @return the result of the addition
+    */
+  def sum(s: Sign, t: Sign) : Sign =
     (s,t) match {
       case (SignBottom, _) => SignBottom
       case (_, SignBottom) => SignBottom
@@ -41,9 +65,14 @@ object SignDomainCore {
       case (Minus, Minus) => Minus
       case _ => SignTop
     }
-  }
 
-  def mult(s: Sign, t : Sign) : Sign = {
+  /**
+    * Returns the multiplication of two sign variables.
+    * @param s the first factor of the multiplication
+    * @param t the second factor of the multiplication
+    * @return the result of the multiplication
+    */
+  def mult(s: Sign, t : Sign) : Sign =
     (s,t) match {
       case (SignBottom, _) => SignBottom
       case (_, SignBottom) => SignBottom
@@ -53,17 +82,26 @@ object SignDomainCore {
       case (_, SignTop) => SignTop
       case (a, b) => if(a == b) Plus else Minus
     }
-  }
 
-  def inverse(s: Sign) : Sign = {
+  /**
+    * Performs the (-) prefix operation.
+    * @param s sign that will be inverted
+    * @return the inverse of `s`
+    */
+  def inverse(s: Sign) : Sign =
     s match {
       case Plus => Minus
       case Minus => Plus
       case a => a
     }
-  }
 
-  def division(s : Sign, t : Sign) : Sign = {
+  /**
+    * Returns the division of two sign variables.
+    * @param s the numerator of the division
+    * @param t the denominator of the division
+    * @return the result of the division
+    */
+  def division(s : Sign, t : Sign) : Sign =
     (s, t) match {
       case (SignBottom, _) => SignBottom
       case (_, SignBottom) => SignBottom
@@ -73,10 +111,14 @@ object SignDomainCore {
       case (_, SignTop) => SignTop
       case (a, b) => if (a == b) Plus else Minus
     }
-  }
 
-
-  def remainder(s : Sign, t : Sign) : Sign = {
+  /**
+    * Returns the result of the modulo operation between two sign variables.
+    * @param s number put under modulo operation
+    * @param t modulus
+    * @return the remainder of the modulo operation
+    */
+  def remainder(s : Sign, t : Sign) : Sign =
     (s,t) match {
       case (SignBottom, _) => SignBottom
       case (_, SignBottom) => SignBottom
@@ -84,9 +126,14 @@ object SignDomainCore {
       case (Zero, _) => Zero
       case (_, _) => SignTop
     }
-  }
 
-  def lub(s : Sign, t : Sign) : Sign = {
+  /**
+    * Returns the least upper bound between two sign variables.
+    * @param s first term of the lub
+    * @param t second term of the lub
+    * @return the lub of `s` and `t`
+    */
+  def lub(s : Sign, t : Sign) : Sign =
     (s, t) match {
       case (SignTop, _) => SignTop
       case (_, SignTop) => SignTop
@@ -94,9 +141,14 @@ object SignDomainCore {
       case (a, SignBottom) => a
       case (a, b) => if (a == b) a else SignTop
     }
-  }
 
-    def glb(s : Sign, t : Sign) : Sign = {
+  /**
+    * Returns the greatest lower bound between two sign variables.
+    * @param s first term of the glb
+    * @param t second term of the glb
+    * @return the glb of `s` and `t`
+    */
+  def glb(s : Sign, t : Sign) : Sign =
       (s,t) match {
         case (SignTop, a) => a
         case (a, SignTop) => a
@@ -104,28 +156,22 @@ object SignDomainCore {
         case (SignBottom, _) => SignBottom
         case (a, b) => if(a == b) a else SignBottom
       }
-    }
 
-
-    def compare(s: Sign, t:Sign): Option[Int] = {
-      (s, t) match {
-        case (SignTop, SignTop) => Option(0)
-        case (SignTop, _) => Option(1)
-        case (_, SignTop) => Option(-1)
-        case (SignBottom, SignBottom) => Option(0)
-        case (SignBottom, _) => Option(-1)
-        case (_, SignBottom) => Option(1)
-        case (a, b) => if (a.equals(b)) Option(0) else Option.empty
-      }
-    }
-
-
-
-  /*** XOR DISCLAIMER: If we do not distinguish between >= 0, and > 0 then
-    * we take top because
-    * 10 ^ 10 = 0
-    * 10 ^ 11 = 1
-    * therefore with + + we obtain top
+  /**
+    * Performs a Java-like comparison (same behaviour as Java's compareTo) between two signs.
+    * @param s left hand side
+    * @param t right hand side
+    * @return 1 if `s` > `t` -- 0 if `s` = `t` -- -1 if `s` < `t`
     */
+  def compare(s: Sign, t:Sign): Option[Int] =
+    (s, t) match {
+      case (SignTop, SignTop) => Option(0)
+      case (SignTop, _) => Option(1)
+      case (_, SignTop) => Option(-1)
+      case (SignBottom, SignBottom) => Option(0)
+      case (SignBottom, _) => Option(-1)
+      case (_, SignBottom) => Option(1)
+      case (a, b) => if (a.equals(b)) Option(0) else Option.empty
+    }
 
-} // end object SignFunctions
+} // end object SignDomainCore

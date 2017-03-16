7d1362c2ddde4b279ce0cf955124a65735218adc
Sebastiano <sebastiano.valle@studenti.unipd.it>
2017-03-16 18:49:14 +0100

    Refactor ES01 domain by extending BND

--- core/src/main/scala/it/unipd/jandom/domains/numerical/sign/ExtendedSigns01Domain.scala
+++ core/src/main/scala/it/unipd/jandom/domains/numerical/sign/ExtendedSigns01Domain.scala
@@ -1,112 +1,36 @@
 package it.unipd.jandom.domains.numerical.sign
 
-import it.unich.jandom.domains.WideningDescription
-import it.unich.jandom.domains.numerical.{LinearForm, NumericalDomain, NumericalProperty}
-import it.unich.jandom.utils.numberext.RationalExt
+import it.unich.jandom.domains.numerical.LinearForm
+import it.unipd.jandom.domains.numerical.BaseNumericalDomain
 import it.unipd.jandom.domains.numerical.sign.ES01._
-import spire.math.Rational
 
 /**
-  * Sign domain extended with the 0 and 1 constants.
+  * Extended sign domain with 0 and 1, i.e. the domain composed of the elements Negative (negative numbers), GTOne
+  * (numbers greater than one), Zero (0) and One (1). SignTop and SignBottom complete the lattice, providing a greatest
+  * and a least element for this set.
+  *
+  * @author Mirko Bez <mirko.bez@studenti.unipd.it>
+  * @author Stefano Munari <stefano.munari@studenti.unipd.it>
+  * @author Sebastiano Valle <sebastiano.valle@studenti.unipd.it>
   */
-object ExtendedSigns01Domain extends NumericalDomain {
+class ExtendedSigns01Domain
+  extends BaseNumericalDomain[ExtendedSign01, ExtendedSigns01DomainCore.type](ExtendedSigns01DomainCore) {
 
-  import ExtendedSigns01DomainCore._
+  // this class uses the operations defined in ExtendedSign01DomainCore
 
-  case class Property private[ExtendedSigns01Domain](sign : Array[ExtendedSign01], unreachable : Boolean) extends NumericalProperty[Property] {
-    val dimension: Int = sign.length
-    require(dimension >= 0)
+  /**
+    * @inheritdoc
+    */
+  override def createProperty(signsArray : Array[ExtendedSign01], unreachable : Boolean): Property =
+    Property(signsArray, unreachable)
 
-    type Domain = ExtendedSigns01Domain.type
-
-    def domain = ExtendedSigns01Domain
-
-    def tryCompareTo[B >: Property](that: B)(implicit evidence$1: (B) => PartiallyOrdered[B]): Option[Int] = that match {
-
-      case that: Property =>
-        require(dimension == that.dimension)
-        (isEmpty, that.isEmpty) match {
-          case (true, true) => Option(0)
-          case (false, true) => Option(1)
-          case (true, false) => Option(-1)
-          case (false, false) =>
-            println(s"s: $this   --- t: $that")
-            val signPairs = (this.sign, that.sign).zipped
-            val comparisonList = signPairs.map(compare)
-
-            if (comparisonList.forall {
-              case Some(i) => if (i == 0) true else false
-              case None => false
-            })
-              Option(0)
-            else if (comparisonList.forall {
-              case Some(i) => if (i >= 0) true else false
-              case None => false
-            } && comparisonList.exists {
-              case Some(i) => if (i == 1) true else false
-              case None => false
-            })
-              Option(1)
-            else if (comparisonList.forall {
-              case Some(i) => if (i <= 0) true else false
-              case None => false
-            } && comparisonList.exists {
-              case Some(i) => if (i == -1) true else false
-              case None => false
-            })
-              Option(-1)
-            else
-              Option.empty
-        }
-
-      case _ => None
-    }
-
-    override def union(that: Property): Property =
-      Property((this.sign, that.sign).zipped.map(lub), unreachable && that.unreachable)
-
-    override def intersection(that: Property) : Property =
-      ExtendedSigns01Domain.this ((this.sign, that.sign).zipped.map(glb))
-
-    override def widening(that: Property): Property = union(that)
-
-    override def narrowing(that: Property): Property = this
-
-    override def nonDeterministicAssignment(n: Int): Property =
-      if (unreachable)
-        this
-      else
-        Property(sign.updated(n, ES01Top), unreachable = false)
-
-    private def linearEvaluation(lf : LinearForm) : ExtendedSign01 = {
-      val known = lf.known.toDouble.toInt
-      val homCoeffs =lf.homcoeffs.map (_.toDouble.toInt).toArray
-      linearEvaluation(known, homCoeffs)
-    }
-
-    private def linearEvaluation(known : Int, homCoeffs : Array[Int]) : ExtendedSign01 = {
-      require(homCoeffs.length <= dimension)
-      var s : ExtendedSign01 = alpha(known)
-      if  (unreachable && homCoeffs.exists { _ != 0} ) return ES01Top
-      for (i <- homCoeffs.indices) {
-        if (homCoeffs(i) > 0) {
-          val t: ExtendedSign01 = sign(i)
-          s = sum(s, t)
-        } else if (homCoeffs(i) < 0) {
-          val t: ExtendedSign01 = inverse(sign(i))
-          s = sum(s, t)
-        }
-      }
-      s
-    }
-
-    def linearAssignment(pos : Int, lf : LinearForm) : Property = {
-      require(pos < sign.length && pos >= 0 && lf.dimension <= dimension)
-      if(unreachable)
-        return this
-      val s : ExtendedSign01 = linearEvaluation(lf)
-      ExtendedSigns01Domain.this (sign.updated(pos, s))
-    }
+  /**
+    * Numerical property that describes the extended sign with 0,1 of the variables in a certain point of the CFG.
+    * @param sign array of the variables' signs
+    * @param unreachable tells if a given program point is unreachable
+    */
+  case class Property private[ExtendedSigns01Domain](sign : Array[ExtendedSign01], override val unreachable : Boolean)
+    extends BaseProperty(sign, unreachable) {
 
     /**
       * @inheritdoc
@@ -141,77 +65,11 @@ object ExtendedSigns01Domain extends NumericalDomain {
       }
     }
 
-    def minimize(lf: LinearForm): RationalExt =
-      if (lf.homcoeffs.exists(!_.isZero))
-        RationalExt.NegativeInfinity
-      else
-        RationalExt(lf.known)
-
-    def maximize(lf: LinearForm): RationalExt =
-      if (lf.homcoeffs.exists(!_.isZero))
-        RationalExt.PositiveInfinity
-      else
-        RationalExt(lf.known)
-
-    def frequency(lf: LinearForm): Option[Rational] =
-      if (lf.homcoeffs.exists(!_.isZero))
-        Option.empty
-      else
-        Option(lf.known)
-
-    def constraints = List()
-
-    def isPolyhedral = false
-
-    /**
-      * @inheritdoc
-      */
-    def addVariable(): Property = {
-      if (unreachable)
-        return ExtendedSigns01Domain.this.bottom(dimension + 1)
-      ExtendedSigns01Domain.this(sign :+ ES01Top)
-    }
-
-    /**
-      * @inheritdoc
-      */
-    def delVariable(pos: Int): Property = {
-      require(pos < sign.length && pos >= 0)
-      val newSign = new Array[ExtendedSign01](sign.length - 1)
-      // Copy the first pos-1 elements
-      Array.copy(sign, 0, newSign, 0, pos)
-      // Copy the remaining elements
-      Array.copy(sign, pos + 1, newSign, pos, sign.length - pos - 1)
-      Property(newSign, unreachable)
-    }
-
-    /**
-      * @inheritdoc
-      */
-    def mapVariables(rho: Seq[Int]): Property = {
-      require(rho.length == dimension)
-      val newdim = rho.count(_ >= 0)
-      require(rho forall { i => i >= -1 && i < newdim })
-      // we do not check injectivity
-      val newSign = new Array[ExtendedSign01](newdim)
-      for ((newi, i) <- rho.zipWithIndex; if newi >= 0)
-        newSign(newi) = sign(i)
-      Property(newSign, unreachable)
-    }
-
-    override def isEmpty : Boolean = unreachable
-
-    override def isTop : Boolean = !isEmpty && sign.forall( _.equals(ES01Top))
-    override def isBottom : Boolean = isEmpty
-
-    def bottom : Property = ExtendedSigns01Domain.bottom(sign.length)
-    def top : Property = ExtendedSigns01Domain.top(sign.length)
-
     /**
       * @inheritdoc
     // * @throws IllegalArgumentException
       */
-    def mkString(vars: Seq[String]): String = {
+    override def mkString(vars: Seq[String]): String = {
       require(vars.length >= dimension)
       if (unreachable)
         "empty"
@@ -230,47 +88,12 @@ object ExtendedSigns01Domain extends NumericalDomain {
         bounds.mkString("[ ", " , ", " ]")
       }
     }
+  } // end of ES01Domain's Property
+} // end of ES01Domain's class
 
-    /**
-      * @inheritdoc
-      */
-    override def variableMul(n: Int, m: Int): Property = {
-      sign(n) = mult(sign(n), sign(m))
-      this
-    }
-
-    /**
-      * @inheritdoc
-      */
-    override def variableDiv(n : Int, m : Int): Property = {
-      sign(n) = division(sign(n), sign(m))
-      this
-    }
-
-    /**
-      * @inheritdoc
-      */
-    override def variableNeg(n: Int = dimension - 1): Property = {
-      sign(n) = inverse(sign(n))
-      this
-    }
-
-    /**
-      * @inheritdoc
-      */
-    override def variableRem(n: Int = dimension - 2, m: Int = dimension - 1): Property = {
-      sign(n) = remainder(sign(n), sign(m))
-      this
-    }
-
-  } // end Property
-
-  def apply(signArray : Array[ExtendedSign01]) : Property =
-    Property(signArray, signArray.forall( _.equals(ES01Bottom)))
-
-  val widenings = Seq(WideningDescription.default[Property])
-
-  def top(n: Int) = Property(Array.fill(n)(ES01Top), unreachable = false)
-  def bottom(n: Int) = Property(Array.fill(n)(ES01Bottom), unreachable = true)
-
-}
+object ExtendedSigns01Domain {
+  /**
+    * Ctor for Exte
+    */
+  def apply(): ExtendedSigns01Domain = new ExtendedSigns01Domain()
+} // end of ES01Domain's companion object

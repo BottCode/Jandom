ec2bfd731d496df9f71f0f2e72b5f5347299409f
StefanoMunari <stefanomunari.sm@gmail.com>
2017-03-18 12:15:32 +0100

    refactoring - move ES01 object in a separate file

--- core/src/main/scala/it/unipd/jandom/domains/numerical/sign/ExtendedSigns01DomainCore.scala
+++ core/src/main/scala/it/unipd/jandom/domains/numerical/sign/ExtendedSigns01DomainCore.scala
@@ -1,23 +1,6 @@
 package it.unipd.jandom.domains.numerical.sign
 
 import it.unipd.jandom.domains.{Abstraction, CompleteLatticeOperator, IntOperator}
-
-object ES01 {
-  trait ExtendedSign01
-  // negative numbers (< 0)
-  case object Negative extends ExtendedSign01
-  // null numbers (= 0)
-  case object Zero extends ExtendedSign01
-  // numbers equal to 1 (= 1)
-  case object One extends ExtendedSign01
-  // numbers greater than 1 (> 1)
-  case object GTOne extends ExtendedSign01
-  // no accurate info available for variable
-  case object ES01Top extends ExtendedSign01
-  // no possible value
-  case object ES01Bottom extends ExtendedSign01
-}
-
 import ES01._
 
 /**

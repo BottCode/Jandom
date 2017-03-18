4fe59c99d6cdd13ea662ffe37d98974ee9e9d367
StefanoMunari <stefanomunari.sm@gmail.com>
2017-03-18 11:38:32 +0100

    refactoring - move Parity object in another file

---
+++ core/src/main/scala/it/unipd/jandom/domains/numerical/parity/Parity.scala
@@ -0,0 +1,13 @@
+package it.unipd.jandom.domains.numerical.parity
+
+object Parity {
+  trait Parity
+  // multiples of 2
+  case object Even extends Parity
+  // multiples of 2 plus 1
+  case object Odd extends Parity
+  // no accurate info available for variable
+  case object ParityTop extends Parity
+  // no possible value
+  case object ParityBottom extends Parity
+}

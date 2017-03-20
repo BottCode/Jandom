a46ac8e559c3dc76130a7680d51d6143f4252a3d
Mirko Bez <mirko.bez@studenti.unipd.it>
2017-03-20 17:43:12 +0100

    Rewritten EseqDomain as subclass of BaseNumericalDomain

--- core/src/main/scala/it/unipd/jandom/domains/numerical/sign/ESeqDomain.scala
+++ core/src/main/scala/it/unipd/jandom/domains/numerical/sign/ESeqDomain.scala
@@ -1,6 +1,7 @@
 package it.unipd.jandom.domains.numerical.sign
 
 import it.unich.jandom.domains.numerical.LinearForm
+import it.unipd.jandom.domains.numerical.BaseNumericalDomain
 import it.unipd.jandom.domains.numerical.sign.Sign._
 import it.unipd.jandom.domains.numerical.sign.ESeq._
 
@@ -13,21 +14,58 @@ import it.unipd.jandom.domains.numerical.sign.ESeq._
   * @author Stefano Munari <stefano.munari@studenti.unipd.it>
   * @author Sebastiano Valle <sebastiano.valle@studenti.unipd.it>
   */
-class ESeqDomain extends SignDomain {
+class ESeqDomain extends BaseNumericalDomain[Sign, ESeqDomainCore](ESeqDomainCore()) {
 
   override def createProperty(elements: Array[Sign], unreachable: Boolean): Property =
     new Property(elements, unreachable)
+
+  type Property = ESeqDomainProperty
 
   /**
     * Property of a point in the CFG for the ESeq domain.
     * @param sign array of sign variables
     * @param unreachable tells if the point of the CFG is unreachable
     */
-  class ESeqDomainProperty (sign : Array[Sign], unreachable : Boolean) extends SignProperty(sign, unreachable) {
+  class ESeqDomainProperty (sign : Array[Sign], unreachable : Boolean) extends BaseProperty(sign, unreachable) {
 
     self: Property =>
-    
-  } // end class Property
+    /**
+      * Intersection with the half-plane `lf <= 0`.
+      *
+      * @todo $TODOGEN
+      */
+    override def linearInequality(lf: LinearForm): Property = {
+      val s : Sign = linearEvaluation(lf)
+      if (isEmpty)
+        return this
+      s match {
+        case Plus => bottom
+        case SignBottom => bottom
+        case _ => this
+      }
+    }
+
+    /**
+      * Intersection with `lf != 0`.
+      *
+      * @todo $TODOGEN
+      */
+    override def linearDisequality(lf: LinearForm): Property = {
+      if (isEmpty)
+        return this
+      val s : Sign = linearEvaluation(lf)
+      s match {
+        case Zero => bottom
+        case SignBottom => bottom
+        case _ => this
+      }
+    }
+} // end class Property
+  /**
+    * @inheritdoc
+    * For numerical domains, properties needs to be instances of [[it.unich.jandom.domains.NumericalProperty]].
+    */
+
 } // end class ESeqDomain
 
 object ESeqDomain {

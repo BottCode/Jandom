0e1cf19ffc8527770dcb165ddcf02430ed3fc5f5
Mirko Bez <mirko.bez@studenti.unipd.it>
2017-03-20 21:54:59 +0100

    Create congruence class separated from the core

--- core/src/main/scala/it/unipd/jandom/domains/numerical/congruence/CongruenceDomainCore.scala
+++ core/src/main/scala/it/unipd/jandom/domains/numerical/congruence/CongruenceDomainCore.scala
@@ -2,14 +2,6 @@ package it.unipd.jandom.domains.numerical.congruence
 
 import it.unipd.jandom.domains.{Abstraction, CompleteLatticeOperator, IntOperator}
 
-trait Congruence
-case class Mod(a : Option[Int], b : Int) extends Congruence {
-  require(a match { // Checking if the value is bigger than 0 as in Mine02
-    case None => true
-    case Some(x) => x > 0
-  })
-}
-case object CongruenceBottom extends Congruence //Corresponds to the empty set
 
 /**
   * Created by mirko on 3/9/17.

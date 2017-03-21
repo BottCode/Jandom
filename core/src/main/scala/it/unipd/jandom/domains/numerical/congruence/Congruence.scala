705ec0f1ded3d8bb76f8a6e9b5477ab483ecef9d
StefanoMunari <stefanomunari.sm@gmail.com>
2017-03-21 19:03:20 +0100

    refactoring - Congruence: wrap trait in an singleton object; add docs

--- core/src/main/scala/it/unipd/jandom/domains/numerical/congruence/Congruence.scala
+++ core/src/main/scala/it/unipd/jandom/domains/numerical/congruence/Congruence.scala
@@ -1,27 +1,37 @@
 package it.unipd.jandom.domains.numerical.congruence
 
-trait Congruence
-case class Mod(a : Option[Int], b : Int) extends Congruence {
+/**
+  * The elements of the congruence domain.
+  * It is a non-relational domain composed of values in the form `aZ + b` (Mod)
+  *
+  * @author Mirko Bez <mirko.bez@studenti.unipd.it>
+  * @author Stefano Munari <stefano.munari.1@studenti.unipd.it>
+  * @author Sebastiano Valle <sebastiano.valle@studenti.unipd.it>
+  */
+object Congruence {
+  trait Congruence
+  case class Mod(a : Option[Int], b : Int) extends Congruence {
+    // Checking if the value is bigger than 0
+    require(
+      a match {
+        case None => true
+        case Some(x) => x > 0
+      })
 
-  require(a match {
-    // Checking if the value is bigger than 0 as in Mine02
-    case None => true
-    case Some(x) => x > 0
-  })
-
-  override def toString: String = {
-    (a, b) match {
-      case (None, 0) => "0"
-      case (None, x) => x + ""
-      case (Some(1), 0) => "Z"
-      case (Some(1), x) => "Z + " + x
-      case (Some(a), 0) => a + "Z"
-      case (Some(a), x) => a + "Z + " + x
+    override def toString: String = {
+      (a, b) match {
+        case (None, 0) => "0"
+        case (None, _) => b + ""
+        case (Some(1), 0) => "Z"
+        case (Some(1), _) => "Z + " + b
+        case (Some(a), 0) => a + "Z"
+        case (Some(a), _) => a + "Z + " + b
+      }
     }
   }
-}
-case object CongruenceBottom extends Congruence {  //Corresponds to the empty set
-  override def toString: String = "\u22A5"
-}
+  // Corresponds to the empty set
+  case object CongruenceBottom extends Congruence {  
+    override def toString: String = "\u22A5"
+  }
 
-
+}

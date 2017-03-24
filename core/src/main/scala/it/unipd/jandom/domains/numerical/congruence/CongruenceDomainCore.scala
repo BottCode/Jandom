e2b3d0a8ac9f0ff949ed30ff360335969aa0a4d9
StefanoMunari <stefanomunari.sm@gmail.com>
2017-03-25 00:25:09 +0100

    comment methods and class, add authors

--- core/src/main/scala/it/unipd/jandom/domains/numerical/congruence/CongruenceDomainCore.scala
+++ core/src/main/scala/it/unipd/jandom/domains/numerical/congruence/CongruenceDomainCore.scala
@@ -14,6 +14,10 @@ import Congruence._
   * Based on:
   * [[http://www.dsi.unive.it/~avp/domains.pdf]]
   * [[https://www-apr.lip6.fr/~mine/publi/article-mine-sas02.pdf]]
+  *
+  * @author Mirko Bez <mirko.bez@studenti.unipd.it>
+  * @author Stefano Munari <stefano.munari.1@studenti.unipd.it>
+  * @author Sebastiano Valle <sebastiano.valle@studenti.unipd.it>
   */
 class CongruenceDomainCore extends CompleteLatticeOperator[Congruence]
   with IntOperator[Congruence] with Abstraction[Int, Congruence]{
@@ -23,6 +27,13 @@ class CongruenceDomainCore extends CompleteLatticeOperator[Congruence]
     */
   override def alpha(num: Int): Congruence =  standardForm(Mod(None, num))
 
+  /**
+    * overloaded abstraction function: 
+    * it is necessary to convert all the aZ+b formS 
+    * in standard forms a'Z+b' with a' > 0 and b' < a'.
+    * All the methods of this class have to use alpha 
+    * to construct abstract elements of this domain in the standard form.
+    */
   def alpha(a: Option[Int], b: Int): Congruence =  standardForm(Mod(a, b))
 
   /**
@@ -51,19 +62,18 @@ class CongruenceDomainCore extends CompleteLatticeOperator[Congruence]
 
   /**
     * @inheritdoc
+    * @note 0Z+0 which Miné represents as Inf*Z+0 denotes the singleton {0}
+    *       We need to distinguish the cases with 0Z+0 from the others 
+    *       to avoid computing gcd(0,0) which will result in a runtime error
     */
   def mult(c : Congruence, d : Congruence) : Congruence = {
     (c, d) match {
       case (CongruenceBottom, _) => CongruenceBottom
       case (_, CongruenceBottom) => CongruenceBottom
-      case (Mod(None, 0), _) => Mod(None, 0) //0 * something = 0
-      case (_, Mod(None, 0)) => Mod(None, 0) //something * 0 = 0
+      case (Mod(None, 0), _) => Mod(None, 0) //{0} * something = {0}
+      case (_, Mod(None, 0)) => Mod(None, 0) //something * {0} = {0}
       case (Mod(None, c0), Mod(None, c1)) => Mod(None, c0 * c1) //Multiplication between constants
       case (Mod(a0, b0), Mod(a1, b1)) =>
-        /* note:
-           gcd(a0*a1, _) must be the first operation performed
-           because b0 and b1 could be equal to 0 which implies
-           gcd(0,0) which results in a runtime error */
         val a = M.gcd(M.*(a0, Some(b1)),
           M.gcd(M.*(a0, a1), M.*(a1, Some(b0))))
         val b = b0 * b1
@@ -124,6 +134,18 @@ class CongruenceDomainCore extends CompleteLatticeOperator[Congruence]
 
   /**
     * @inheritdoc
+    * @note (a0Z + b0) ∩ (a1Z + b1)=
+    *  {gcd(a0,a1)Z+b2  if b0 ≡ b1 lcm(a0,a1) //[caseA]
+    *  {⊥               otherwise
+    * how to compute b2 must be valid ([caseA] holds):
+    *
+    *   if (a0,a1) are coprimes then b2 is computed using the
+    *   Bezout theorem and the Chinese remainder theorem solving the system
+    *   { b2 ≡ b0 mod a0
+    *   { b2 ≡ b1 mod a1
+    *
+    *   if (a0,a1) are NOT coprimes, then there is an intersection between the
+    *   two congruence only if the constants are equal (b0==b1). So b2=b0. 
     */
   def glb(c : Congruence, d : Congruence) : Congruence = {
     (c,d) match {
@@ -142,7 +164,12 @@ class CongruenceDomainCore extends CompleteLatticeOperator[Congruence]
           if(M.gcd(a0,a1).contains(1)){
             var (x0, x1, y0, y1) = (a0.get, a1.get, b0, b1)
 
-            if(Math.max(a0.get,a1.get) == a0.get){ //Swap values in order to have only one case to distinguish
+            if(Math.max(a0.get,a1.get) == a0.get){ 
+              /*
+              Swap values in order to have only one case to distinguish:
+              x1 contains the maximum and x0 the minimum, 
+              swap the b1,b0 accordingly
+              */
               x1=a0.get
               x0=a1.get
               y0=b1
@@ -153,6 +180,7 @@ class CongruenceDomainCore extends CompleteLatticeOperator[Congruence]
             val x= y0*bezout0*x1+y1*bezout1*x0
             alpha(a,x)
           }
+          /* !coprime(a0,a1) case */
           else if(b0 == b1)
             alpha(a, b0)
           else
@@ -194,6 +222,8 @@ class CongruenceDomainCore extends CompleteLatticeOperator[Congruence]
 
   /**
     * @inheritdoc
+    * @note it is necessary to use alpha because it generates abstract elements
+    * in the standard form
     */
   override def top: Congruence = alpha(Some(1),0)
 
@@ -204,7 +234,7 @@ class CongruenceDomainCore extends CompleteLatticeOperator[Congruence]
 
   /**
     * Reduce the congruence abstract domain, thus making (alpha, C, A, gamma)
-    * a Galois Insertion.
+    * a Galois Insertion. So gamma becomes an injective function.
     * aZ+b with a > 0 and a > b
     */
   private def standardForm(c : Congruence) : Congruence = {

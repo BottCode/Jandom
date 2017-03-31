96e5199982d718f4da043c7725adfe77a0430fce
StefanoMunari <stefanomunari.sm@gmail.com>
2017-03-31 20:29:01 +0200

    add an improved version of the algorithm which computes the restricted intervals of FullyReducedProductCongruenceBoxDouble; add javadoc to all methods; improve 'transform' method readability;

--- core/src/main/scala/it/unipd/jandom/domains/numerical/FullyReducedProductCongruenceBoxDoubleDomain.scala
+++ core/src/main/scala/it/unipd/jandom/domains/numerical/FullyReducedProductCongruenceBoxDoubleDomain.scala
@@ -32,7 +32,9 @@ class FullyReducedProductCongruenceBoxDoubleDomain(override val dom1 : Congruenc
     */
   class FullyReducedProductCongruenceBoxDouble(override val p1 : dom1.Property, override val p2 : dom2.Property) extends Product(p1, p2) {
 
-
+    /**
+    * @inheritdoc
+    */
     override def reduce(x1: dom1.Property, x2: dom2.Property): FullyReducedProductCongruenceBoxDouble = {
       //Checking if the properties are empty, or contain a bottom element
       if(x1.isEmpty || x2.isEmpty || x1.elements.contains(CongruenceBottom) || (x2.low, x2.high).zipped.exists(_ > _))
@@ -70,22 +72,71 @@ class FullyReducedProductCongruenceBoxDoubleDomain(override val dom1 : Congruenc
     }
 
     /**
-      * Function that takes as input an interval [_low, _high] and an interval aZ + b and returns
-      * the fully reduced product between low, high and aZ + b
+      * This function that takes as input an interval [_low, _high] and 
+      * an interval aZ + b. It returns the fully reduced product between 
+      * [low, high] and aZ + b. Thus, reducing [_low, _high] using aZ + b
+      * @param _low the lower bound of the interval
+      * @param _high the upper bound of the interval
+      * @param a the module part of the congruence aZ+b
+      * @param b the constant part of the congruence aZ+b
+      * @returns (low, high) the (potentially) restricted interval
       */
     private def transform(_low : Int, _high: Int, a : Option[Int], b : Int) : (Int, Int) = {
       var (low, high) = (_low, _high)
 
-      if (!MathLibrary.isCongruent(low, b, a) || !MathLibrary.isCongruent(high, b, a)) {
-        /* restrict the interval until it matches with the congruence */
-        while (!MathLibrary.isCongruent(low, b, a))
-          low = low + 1
-        while (!MathLibrary.isCongruent(high, b, a))
-          high = high - 1
-        (low, high)
+      if (!MathLibrary.isCongruent(low, b, a) || 
+          !MathLibrary.isCongruent(high, b, a)) 
+      {
+          if(!MathLibrary.isCongruent(low, b, a))
+            low = compute_low(low,a,b)
+          if(!MathLibrary.isCongruent(high, b, a))
+            high = compute_high(high,a,b)
       }
-      else /* preserve the interval */
-        (_low, _high)
+      (low, high)
+    }
+  }
+
+/**
+  * Computes the (potentially) new upper bound of the interval.
+  * If the congruence is a constant then the interval has to match the constant value.
+  * Otherwise compute the gap between old_high and the lowest value
+  * of the congruence closer to old_high. Then substract it to old_high. 
+  * The gap is computed as following: 
+  * 1. compute the (| normalized_rest - b | % a) 
+  *    which is the rest which makes old_high not congruent to b mod a.
+  *    Note that this represents the gap between old_high and the nearest
+  *    higher value of the congruence, but we are searching the lowest one.
+  * 2. subtract highest_value_gap to a, thus obtaining the gap between the 
+  *    lowest value of the congruence closer to old_high and old_high
+  * 3. substract lowest_value_gap to old_high to obtain new high
+  * @param high the upper bound of the interval
+  * @param a0 the module part of the congruenze a0Z+b1
+  * @param b1 the constant part of the congruence a0Z+b1
+  * @return the new (possibily restriced) upper bound of the interval
+  */
+  private def compute_high (high : Int, a0 : Option[Int], b1 : Int) = {
+    a0 match {
+      case None     => b1
+      case Some(a1) => high - (a1 - (Math.abs((((high % a1)+ a1)% a1) - b1)% a1))
+    }
+  }
+
+/**
+  * Computes the (potentially) new lower bound of the interval.
+  * If the congruence is a constant then the interval has to match the constant value.
+  * Otherwise compute the gap between old_low and the highest value
+  * of the congruence closer to old_low. Then sum it to old_low.
+  * The logic is similar to compute_high
+  * @see compute_high
+  * @param low the lower bound of the interval 
+  * @param a0 the module part of the congruenze a0Z+b1
+  * @param b1 the constant part of the congruence a0Z+b1
+  * @return the new (possibily restriced) lower bound of the interval
+  */
+  private def compute_low (low : Int, a0 : Option[Int], b1 : Int) = {
+    a0 match {
+      case None     => b1
+      case Some(a1) => low + (Math.abs((((low % a1)+ a1)% a1) - b1)% a1)
     }
   }
 

95db44b398d0cdb678b74b8e421d7f7dd1206a4e
herrBez <mirko.bez@studenti.unipd.it>
2017-12-30 18:41:21 +0100

    Add license notice to each scala file in unipd subfolder

--- core/src/main/scala/it/unipd/jandom/domains/numerical/constant/ConstantDomainCore.scala
+++ core/src/main/scala/it/unipd/jandom/domains/numerical/constant/ConstantDomainCore.scala
@@ -1,3 +1,20 @@
+/**
+ * Copyright 2017 Mirko Bez, Stefano Munari, Sebastiano Valle
+ *
+ * This file is part of JANDOM: JVM-based Analyzer for Numerical DOMains
+ * JANDOM is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * JANDOM is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of a
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You shosuld have received a copy of the GNU General Public License
+ * along with JANDOM.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package it.unipd.jandom.domains.numerical.constant
 
 import it.unipd.jandom.domains.{Abstraction, CompleteLatticeOperator, IntOperator}
